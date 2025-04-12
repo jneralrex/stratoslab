d5c94b6554d05aeedd6c32576912e8e137f56568
Idah Abubakar Isah <idahabubakar49@gmail.com>
2025-04-12 17:35:24 +0100

    Twitter can update

--- src/app/layout.js
+++ src/app/layout.js
@@ -32,7 +32,7 @@ export default function RootLayout({ children }) {
         <meta property="og:type" content="website" />
         <meta property="og:title" content={metadata.title} />
         <meta property="og:description" content={metadata.description} />
-        <meta property="og:image" content="/imagecard.png" />
+        <meta property="og:image" content="https://stratoslab.vercel.app/imagecard.png" />
         <meta property="og:url" content="https://stratoslab.vercel.app/" />
 
         {/* Twitter */}
